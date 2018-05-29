521b99c7d45dc1d72c4d0e3b232e2481c06567f3
jim lesperance <contact@jimlesperance.com>
2018-05-30 00:35:05 +0200

    redirect fake path to home

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import App from './App';
 import Form from './form';
+
 const Routes = () => {
   return (
     <div>
     <Switch>
       <Route path="/" exact component={App} />
       <Route path="/form" exact component={Form} />
+      <Route path="*" render={() => (<Redirect to="/" />)} />
     </Switch>
     </div>
   );
