353b3f0d9dbeca3624a7fac4051845296d40e990
jim lesperance <contact@jimlesperance.com>
2018-05-29 23:19:25 +0200

    add redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import PromiseMiddleware from 'redux-promise';
+import Reducer from './reducers/index';
+import Routes from './routes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const createStoreWithMiddleware = applyMiddleware(
+  PromiseMiddleware
+)(createStore);
+
+ReactDOM.render(
+<Provider store={createStoreWithMiddleware(Reducer)} >
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+</Provider>
+, document.getElementById('root'));
